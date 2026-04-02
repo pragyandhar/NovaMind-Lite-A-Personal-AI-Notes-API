6138636f5eca3ce6c038f1967a454e326edbd645
Pragyan Chandra Dhar <pragyan.dhar_cs.aiml23@gla.ac.in>
2026-04-02 18:05:50 +0530

    Integrate current user dependency in note routes
    Added user authentication dependency to note routes.

--- routers/notes.py
+++ routers/notes.py
@@ -1,6 +1,6 @@
 from app.schemas.note import NoteCreate, NoteResponse
-from app.dependencies import get_db
-from app.models.database import Note
+from app.dependencies import get_db, get_current_user
+from app.models.database import Note, User
 # -------------------------------------
 from sqlalchemy.orm import Session
 from fastapi import Depends
@@ -16,13 +16,13 @@ router = APIRouter(
 
 # List all notes, with skip and limit query params
 @router.get("/", response_model=list[NoteResponse])
-def get_notes(db: Session = Depends(get_db), skip: int = 0, limit: int = 100):
+def get_notes(db: Session = Depends(get_db), current_user: User = Depends(get_current_user), skip: int = 0, limit: int = 100):
     return db.query(Note).offset(skip).limit(limit).all()
 
 
 # Get a single note by ID, return 404 if not found
 @router.get("/{note_id}", response_model=NoteResponse)
-def get_notes_by_id(note_id: int, db: Session = Depends(get_db)):
+def get_notes_by_id(note_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     note = db.query(Note).filter(Note.id == note_id).first()
     if not note:
         raise HTTPException(
@@ -34,7 +34,7 @@ def get_notes_by_id(note_id: int, db: Session = Depends(get_db)):
 
 # Create a note, return 201
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=NoteResponse)
-def create_note(note: NoteCreate, db: Session = Depends(get_db)):
+def create_note(note: NoteCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     new_note = Note(title=note.title, content=note.content, tags=note.tags)
     db.add(new_note)
     db.commit()
@@ -43,14 +43,13 @@ def create_note(note: NoteCreate, db: Session = Depends(get_db)):
 
 
 # Delete a note, return 404 if not found
-@router.delete("/{note_id}")
-def delete_note(note_id: int, db: Session = Depends(get_db)):
-        note = db.query(Note).filter(Note.id == note_id).first()
-        if not note:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="Note Was Not Found"
-            )
-        db.delete(note)
-        db.commit()
-        
+@router.delete("/{note_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_note(note_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    note = db.query(Note).filter(Note.id == note_id).first()
+    if not note:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Note Was Not Found"
+        )
+    db.delete(note)
+    db.commit()
