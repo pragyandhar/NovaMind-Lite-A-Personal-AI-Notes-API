7d8b55766edea6b9077c25b0dc25307aaeb07972
Pragyan Chandra Dhar <pragyan.dhar_cs.aiml23@gla.ac.in>
2026-04-02 18:06:07 +0530

    Implement user authentication in dependencies.py
    Added user authentication and retrieval functionality.

--- dependencies.py
+++ dependencies.py
@@ -1,6 +1,11 @@
 from app.db.session import SessionLocal
+from app.services.auth_service import decode_access_token
+from app.models.database import User
+from app.schemas.user import UserResponse
 # -----------------------------------------
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 # -----------------------------------------
 
 def get_db():
@@ -8,4 +13,15 @@ def get_db():
     try:
         yield db
     finally:        # Handles connection leaks
-        db.close() 
+        db.close()
+    
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    user_id = decode_access_token(token)
+    db_saved_user = db.query(User).filter(User.id == int(user_id)).first()
+    if not db_saved_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="The User Is Not Availble In The Database"
+        )
+    return UserResponse.model_validate(db_saved_user)
