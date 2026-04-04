5c06d64c68492f62f363b9233347636d384180d4
Pragyan Chandra Dhar <pragyan.dhar_cs.aiml23@gla.ac.in>
2026-04-04 18:09:55 +0530

    Implement /summarize/{note_id} endpoint
    Added a new endpoint to summarize notes by ID.

--- routers/ai.py
+++ routers/ai.py
@@ -1,14 +1,24 @@
-from fastapi import APIRouter
-# ----------------------------------
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+# ----------------------------------------
+from app.dependencies import get_current_user, get_db
+from app.models.database import Note, User
+from app.schemas.note import NoteResponse
+from app.services.ai_service import summarize_note
+# ----------------------------------------
 
 router = APIRouter(
     prefix="/ai",
     tags=["AI"]
 )
 
-@router.get("/ping")
-def aiml():
-    return {
-        "service":"ai",
-        "status":"ok"
-    }
+@router.get("/summarize/{note_id}")
+async def aiml(note_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    note = db.query(Note).filter(Note.id == note_id).first()
+    if not note:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No Note Found. Recheck the ID"
+        )
+    summary = await summarize_note(note.title, note.content)
+    return {"summary": summary}
